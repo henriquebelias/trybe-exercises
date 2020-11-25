cbb807903b79bb19f9e5506fee77b807b2b8a9a5
Henrique Brito Elias <henriquebelias@gmail.com>
2020-11-25 18:14:50 -0300

    Exercicio 5

---
+++ bloco_4/dia_1/triangulo.js
@@ -0,0 +1,11 @@
+let a = 60;
+let b = 60;
+let c = 'c';
+
+if (a + b + c == 180) {
+  console.log('true');
+} else if (a + b + c != 180) {
+  console.log('false');
+} else {
+  console.log('Valor inválido');
+}
