68c9b7ca5c88ea2f5298585276bc5bb059f44028
Henrique Brito Elias <henriquebelias@gmail.com>
2020-11-29 15:14:57 -0300

    Exercicio 3

--- bloco_4/dia_3/exercise3.js
+++ bloco_4/dia_3/exercise3.js
@@ -6,3 +6,17 @@
 //   ***
 //  ****
 // *****
+n = 5;
+let result = '';
+
+for (let line = 1; line <= n; line += 1) {
+  for (let column = 0; column < n; column += 1) {
+    if(column < (n - line)) {
+      result += ' ';
+    } else {
+      result += '*';
+    } 
+  }
+  console.log(result);
+  result = '';
+}
--- bloco_4/dia_3/exercise5.js
+++ bloco_4/dia_3/exercise5.js
@@ -1,2 +1,8 @@
 //Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor 
 //de n será sempre ímpar
+// n = 7
+
+//    *
+//   * *
+//  *   *
+// *******
