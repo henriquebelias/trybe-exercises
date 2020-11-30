f4e4180af62d9d936b9ee83c0fae8a5545ce1479
Henrique Brito Elias <henriquebelias@gmail.com>
2020-11-30 18:47:40 -0300

    Exercicio 1 parte II - Funcoes

---
+++ bloco_4/dia_4/exercise2.1.js
@@ -0,0 +1,6 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// Exemplo de palíndromo: arara .
+// verificaPalindrome("arara") ;
+// Retorno esperado: true
+// verificaPalindrome("desenvolvimento") ;
+// Retorno esperado: false
