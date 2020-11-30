cfcc38e250b97d8b74b351549d3c5e59e99931b4
Henrique Brito Elias <henriquebelias@gmail.com>
2020-11-30 15:29:39 -0300

    Refatoracao - Exercicio 10

--- bloco_4/dia_1/produto.js
+++ bloco_4/dia_1/produto.js
@@ -1,6 +1,8 @@
-let valorCusto = 800;
-let valorVenda = 1200;
-let impostoSobreOCusto = 0.2 * valorCusto;
-let valorCustoTotal = valorCusto + impostoSobreOCusto;
+function product(valorCusto, valorVenda) {
+  let impostoSobreOCusto = 0.2 * valorCusto;
+  let valorCustoTotal = valorCusto + impostoSobreOCusto;
+  return valorVenda - valorCustoTotal;
+}
 
-console.log(valorVenda - valorCustoTotal);
+console.log(`O lucro é: ${product(800, 1200)}`);
+console.log(`O lucro é: ${product(1000, 1500)}`);
