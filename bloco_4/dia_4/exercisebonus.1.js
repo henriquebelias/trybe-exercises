ffa25737373e1534c246189f7cb00ad18afea312
Henrique Brito Elias <henriquebelias@gmail.com>
2020-11-30 22:37:50 -0300

    Exercicio bonus 1

---
+++ bloco_4/dia_4/exercisebonus.1.js
@@ -0,0 +1,11 @@
+// Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+let romanos = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
