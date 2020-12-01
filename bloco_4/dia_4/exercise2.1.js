971dadfe76294d2370b9a0893dbb259553d67be2
Henrique Brito Elias <henriquebelias@gmail.com>
2020-12-01 09:27:20 -0300

    Exercicio 1 parte II - Funcoes

--- bloco_4/dia_4/exercise2.1.js
+++ bloco_4/dia_4/exercise2.1.js
@@ -4,3 +4,21 @@
 // Retorno esperado: true
 // verificaPalindrome("desenvolvimento") ;
 // Retorno esperado: false
+function isPalindrome(word) {
+  let reverseWord = '';
+  for (let index = word.length - 1; index >= 0; index -= 1) {
+    reverseWord += word[index];
+  }
+  if (word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome('arara'));
+console.log(isPalindrome('desenvolvimento'));
+console.log(isPalindrome('123321'));
+console.log(isPalindrome('123456'));
+
+
