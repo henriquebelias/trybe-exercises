d4208278a91c50fb16a0f744b427f03225af0174
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-12 18:39:06 -0300

    Parte 2 - Exercicio 1

--- bloco_7/script.js
+++ bloco_7/script.js
@@ -61,3 +61,18 @@ console.log(sortOddsAndEvens());
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+
+//PARTE 2
+
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha.
+
+const fatorial = number => {
+  let result = 1;
+  for (let index = 2; index <= number; index += 1) {
+    result *= index;
+  }
+  return result;
+}
+console.log(fatorial(5));
