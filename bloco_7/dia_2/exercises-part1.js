b5623bfcb5460ea03e98bfbd6ab5a7744805a3f5
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-13 15:42:05 -0300

    Parte 1

--- bloco_7/dia_2/exercises-part1.js
+++ bloco_7/dia_2/exercises-part1.js
@@ -54,6 +54,7 @@ const orderModifier = (order) => {
 
 // Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
 // Note que o parâmetro da função já está sendo passado na chamada da função.
+
 // Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
 // Modifique o nome da pessoa compradora.
 // Modifique o valor total da compra para R$ 50,00.
