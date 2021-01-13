11ebfc69bf6714a933c7418b6f77931441e41f73
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-13 15:41:11 -0300

    Parte 1

---
+++ bloco_7/dia_2/exercises-part1.js
@@ -0,0 +1,59 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      margherita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  console.log( `Olá ${Object.values(order.order.delivery.deliveryPerson).join('')}, entrega para: ${Object.values(order.name).join('')}, Telefone: ${Object.values(order.phoneNumber).join('')}, R. ${Object.values(order.address.street).join('')}, Nº: ${Object.values(order.address.number).join('')}, AP: ${Object.values(order.address.apartment).join('')}`);
+
+}
+
+// customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  order.name = 'Luiz Silva';
+  order.payment.total = 50;
+  console.log(`Olá ${Object.values(order.name).join('')}, o total do seu pedido de ${Object.keys(order.order.pizza).join(', ')} e ${Object.values(order.order.drinks.coke.type).join('')} é R$ ${Object.values(order.payment).join('')},00`);
+}
+
+// orderModifier(order);
+
+
+// Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+// Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
