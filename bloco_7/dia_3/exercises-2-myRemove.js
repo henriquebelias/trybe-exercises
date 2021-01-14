02bdfcefbafcfcfbfd4e5a552d7ca35134c73d15
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-14 19:16:49 -0300

    Exercicio 3 - funcao myRemoveWithoutCopy

--- bloco_7/dia_3/exercises-2-myRemove.js
+++ bloco_7/dia_3/exercises-2-myRemove.js
@@ -12,7 +12,6 @@ function myRemove(arr, item) {
 
 // implemente seus testes aqui
 
-// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
