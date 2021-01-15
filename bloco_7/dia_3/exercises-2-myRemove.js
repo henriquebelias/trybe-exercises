fd6a0dc6929c96189223a613a446e2fce5605659
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-15 09:26:51 -0300

    Exercicio 2 - funcao myRemove - item 5

--- bloco_7/dia_3/exercises-2-myRemove.js
+++ bloco_7/dia_3/exercises-2-myRemove.js
@@ -14,9 +14,14 @@ function myRemove(arr, item) {
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
 // Verifique se o array passado por parâmetro não sofreu alterações
+const myArray = [1, 2, 3, 4];
+myRemove(myArray, 4)
+assert.deepStrictEqual(myArray, [1, 2, 3, 4]);
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
