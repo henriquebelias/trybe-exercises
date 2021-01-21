09cfca8153a5ba252fa5ea95338f173cd6e6961d
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-20 21:21:41 -0300

    Exercicio 5 - sem resolucao

---
+++ bloco_8/dia_3/exercise5.js
@@ -0,0 +1,17 @@
+
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+
+}
+
+// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+// assert.deepStrictEqual(containsA(), 20);
