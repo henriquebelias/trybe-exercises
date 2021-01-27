456a0c2c2667a5ff7cc2c7d2895c514e4750462a
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-27 16:19:19 -0300

    Exercicio 3

---
+++ bloco_9/dia_1/exercise3.js
@@ -0,0 +1,13 @@
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+
+// getPlanet(); // imprime Marte depois de 4 segundos
+setTimeout(() => getPlanet(), 4000);
