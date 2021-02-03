c150feb1ad4a01586b0fa9cc127f08a3bb1e17f4
Henrique Brito Elias <henriquebelias@gmail.com>
2021-02-03 16:16:17 -0300

    Parte 1 - preparando os arquivos

---
+++ bloco_10/dia_1/part-1/exercises-1-sum.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+
+// Teste se o retorno de sum(4, 5) é 9
+assert.strictEqual(sum(4, 5), 9);
+
+// Teste se o retorno de sum(0, 0) é 0
+assert.strictEqual(sum(0, 0), 0);
+
+// Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
+assert.throws(() => {
+  sum(4, '5');
+});
+
+// Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/); // '/ - start of regex' '^ - start of line' '$ - end of line' '/ - end of regex'
