084d5d0465f87806b8d9c3936e83837d55319e25
Henrique Brito Elias <henriquebelias@gmail.com>
2021-02-05 20:22:21 -0300

    Exercicio 5

---
+++ bloco_10/dia_3/exercise5.test.js
@@ -0,0 +1,13 @@
+const modifyStrings = require('./string-functions');
+
+const mockToUpper = jest.spyOn(modifyStrings, 'toUpperCase');
+
+test('modifique a implementação da função, restaure a implementação original e teste', () => {
+  mockToUpper.mockImplementation(string => string.toLowerCase());
+
+  expect(mockToUpper('TESTE')).toBe('teste');
+
+  modifyStrings.toUpperCase.mockRestore();
+
+  expect(modifyStrings.toUpperCase('teste')).toBe('TESTE');
+});
