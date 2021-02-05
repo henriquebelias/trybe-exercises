767498afad907c4ece36e10a1eb4f1ebf7945d3f
Henrique Brito Elias <henriquebelias@gmail.com>
2021-02-05 19:20:45 -0300

    Cria o arquivo com as funçoes para modificar strings

---
+++ bloco_10/dia_3/string-functions.js
@@ -0,0 +1,13 @@
+const toUpperCase = (string) => {
+  return string.toUpperCase();
+}
+
+const returnFirstLetter = (string) => {
+  return string[0];
+}
+
+const concatStrings = (string1, string2) => {
+  return string1 + string2;
+}
+
+module.exports = { toUpperCase, returnFirstLetter, concatStrings }
