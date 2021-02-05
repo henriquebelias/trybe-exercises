6937609ecbeef0c66ac2b871cc7ad660367436e4
Henrique Brito Elias <henriquebelias@gmail.com>
2021-02-05 19:08:17 -0300

    Exercicio 2

---
+++ bloco_10/dia_3/exercise2.test.js
@@ -0,0 +1,11 @@
+const randomNumber = require('./random-number');
+
+const mockRandomNumber = jest.spyOn(randomNumber, 'generateRandomNumber');
+
+test('teste a função, alterando sua implementação uma vez', () => {
+  mockRandomNumber.mockImplementationOnce((num1, num2) => num1 / num2);
+
+  expect(mockRandomNumber(6, 2)).toBe(3);
+  expect(mockRandomNumber).toHaveBeenCalled();
+  console.log(mockRandomNumber());
+});
