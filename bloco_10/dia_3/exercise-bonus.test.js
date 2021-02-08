e212a16b8f1654c10d940383baccff4bae81b654
Henrique Brito Elias <henriquebelias@gmail.com>
2021-02-08 17:33:05 -0300

    Adicionei outras opcoes de resolucao para o exercicio bonus

--- bloco_10/dia_3/exercise-bonus.test.js
+++ bloco_10/dia_3/exercise-bonus.test.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+let fetch = require('node-fetch');
 
 const API_URL = 'https://icanhazdadjoke.com/';
 
@@ -17,8 +17,16 @@ test('teste a chamada feita para um resultado específico', async () => {
     'status': 200
   }
   fetchJoke.mockResolvedValue(result.joke);
+  // fetch = jest.fn(() => Promise.resolve({
+  //   json: () => Promise.resolve(result),
+  // }));
+
 
   expect.assertions(1);
+
+  // return fetchJoke().then(data => expect(data).toBe('Whiteboards ... are remarkable.'));
+
+  // return expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
 
   // const data = await fetchJoke();
   // expect(data).toBe('Whiteboards ... are remarkable.');
