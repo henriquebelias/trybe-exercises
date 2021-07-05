1c548307dcead82c45d239d53989143cc9d71659
Henrique Brito Elias <henriquebelias@gmail.com>
2021-07-05 15:20:18 -0300

    exercicios 27.3

---
+++ bloco_27/dia_3/hello-http-methods/index.js
@@ -0,0 +1,62 @@
+const fetch = require('node-fetch');
+
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+const headers = new fetch.Headers({
+  Authorization: API_TOKEN,
+  'Content-Type': 'application/json',
+});
+
+const body = JSON.stringify({
+  name: 'Tryber',
+  email: 'tryber@betrybe.com',
+  password: 'Tr1b3r',
+})
+
+// GET
+/*
+fetch('https://postman-echo.com/get?param1=teste', {
+  headers,
+})
+  .then((response) => {
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((errorOrResponse) => {
+    if (errorOrResponse.status) {
+      return console.error(`Request failed with status ${errorOrResponse.status}`);
+    }
+
+    console.error(errorOrResponse);
+  });
+*/
+
+// POST
+fetch('https://postman-echo.com/post?param1=teste', {
+  headers,
+  method: 'POST',
+  body,
+})
+  .then((response) => {
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((errorOrResponse) => {
+    if (errorOrResponse.status) {
+      return console.error(`Request failed with status ${errorOrResponse.status}`);
+    }
+
+    console.error(errorOrResponse);
+  });
