4faa92062f8f9847973d3c0bd028957cb095154d
Henrique Brito Elias <henriquebelias@gmail.com>
2021-08-11 10:02:05 -0300

    bloco 31 - dia 2

---
+++ bloco_31/dia_2/sockets_example/server.js
@@ -0,0 +1,28 @@
+/* Importando o pacote NET, responsável pela implementação dos sockets no Node. */
+const net = require('net');
+
+/* Criando o servidor com o método 'createServer', onde recebe uma conexao na qual são expostos os eventos que podemos manipular no nosso servidor. */
+const server = net.createServer((connection) => {
+  console.log('Cliente conectado');
+
+  /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
+  connection.on('end', () => {
+    console.log('Cliente desconectado');
+  });
+  /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
+  // connection.write('Mensagem do servidor!\r\n');
+  // connection.pipe(connection);
+
+  connection.on('data', (data) => {
+    console.log(`O cliente disse: ${data}`);
+  });
+});
+
+server.getConnections((_error, count) => {
+  console.log(count);
+});
+
+/* Após termos programado o servidor, é só colocá-lo de pé */
+server.listen(8080, () => {
+  console.log('Servidor escutando na porta 8080');
+});
